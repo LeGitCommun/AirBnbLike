140c6c3d3d553f92a061fdb19e9925b51951fd16
Veizh <maximedurville95300@gmail.com>
2023-01-19 14:59:40 +0100

    first commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,11 @@
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+     
+      <head />
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import "../styles/home/home.css"
+const  home = () =>{
+    return (
+        <div className="home">
+            Voila le site démarre ici bonne chance Sarah !
+        </div>
+    )
+
+}
+export default home
